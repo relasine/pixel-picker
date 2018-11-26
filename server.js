f33fdc62a5a6c8784b2419992b2d61f2aeb595ed
Kevin Simpson <relasine@gmail.com>
2018-11-26 13:45:20 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+
+app.use(bodyParser.json());
+
+app.set("port", process.env.PORT || 3000);
+
+app.listen(app.get("port"), () => {
+  console.log(`${app.locals.title} is running on ${app.get("port")}`);
+});
