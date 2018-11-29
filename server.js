8bfed866032a9e52da0fced56edaf19a53d3ca03
Kevin Simpson <relasine@gmail.com>
2018-11-29 11:14:50 -0700

    Add heroku stufff

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
-const environment = process.env.NODE_ENV || "development";
+const environment = process.env.NODE_ENV || 'development';
 const configuration = require("./knexfile")[environment];
 const database = require("knex")(configuration);
 
